afef91341ed02a24ca043cc4c0d0702470357038
HudaHajira <hudahajira744@gmail.com>
2024-07-21 21:35:56 -0500

    Add new JavaScript projects

---
+++ Snake-Game./example.js
@@ -0,0 +1,24 @@
+// 1.
+
+function drawGame(){
+    console.log("drae game");
+    setTimeout(drawGame, 1000/speed);
+}
+drawGame();
+// ...................................
+function clearScreen()
+let tileCount=20;
+let headX=10;
+let headY=10;
+let tileSize=(canvas.width/tileCount-2);
+// snake head
+function drawSnake()
+// welocity x and y
+function changeSnakePosition() 
+let appleX=5;
+function DrawApple( )
+function CheckAppleCollision()
+
+class SnakePart{
+    
+}
